dbeac3e6e7f5d0ed7f8bc6750c24c2eb441ba7c8
HaleyB <haleyb128@gmail.com>
2018-09-17 15:38:47 -0500

    another repo to deploy

---
+++ src/pages/noMatch/noMatch.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export class NoMatch extends Component {
+    render(){
+        return (
+            <div>
+                <img src="https://i.imgur.com/wVIm7l8.jpg" className="error-image" id="error-message" alt="Darnit" />
+            </div>
+        );
+    }
+}
